ee005cb95e94a186b4b65852b70f2683948f919c
Fabio Tranchitella <fabio@tranchitella.eu>
2019-11-03 19:59:12 +0100

    Enh: explicit target for tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -4,11 +4,14 @@ from click.testing import CliRunner
 from wazotester import wazotester
 
 
+TARGET = "router:5061"
+
+
 def test_did_ok():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, "scenarios", "test_did_ok.yaml")
     runner = CliRunner()
-    result = runner.invoke(wazotester, [scenario_file])
+    result = runner.invoke(wazotester, [scenario_file, "-t", TARGET])
     assert result.exit_code == 0
 
 
@@ -16,7 +19,7 @@ def test_did_ko():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, "scenarios", "test_did_ko.yaml")
     runner = CliRunner()
-    result = runner.invoke(wazotester, [scenario_file])
+    result = runner.invoke(wazotester, [scenario_file, "-t", TARGET])
     assert result.exit_code == 0
 
 
@@ -24,7 +27,7 @@ def test_domain_ok():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, "scenarios", "test_domain_ok.yaml")
     runner = CliRunner()
-    result = runner.invoke(wazotester, [scenario_file])
+    result = runner.invoke(wazotester, [scenario_file, "-t", TARGET])
     assert result.exit_code == 0
 
 
@@ -32,7 +35,7 @@ def test_domain_ko():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, "scenarios", "test_domain_ko.yaml")
     runner = CliRunner()
-    result = runner.invoke(wazotester, [scenario_file])
+    result = runner.invoke(wazotester, [scenario_file, "-t", TARGET])
     assert result.exit_code == 0
 
 
@@ -40,7 +43,7 @@ def test_outbound_ok():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, "scenarios", "test_outbound_ok.yaml")
     runner = CliRunner()
-    result = runner.invoke(wazotester, [scenario_file])
+    result = runner.invoke(wazotester, [scenario_file, "-t", TARGET])
     assert result.exit_code == 0
 
 
@@ -50,7 +53,7 @@ def test_outbound_ko_no_carrier():
         base_dir, "scenarios", "test_outbound_ko-no-carrier.yaml"
     )
     runner = CliRunner()
-    result = runner.invoke(wazotester, [scenario_file])
+    result = runner.invoke(wazotester, [scenario_file, "-t", TARGET])
     assert result.exit_code == 0
 
 
@@ -58,5 +61,5 @@ def test_outbound_ko_no_ipbx():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, "scenarios", "test_outbound_ko-no-ipbx.yaml")
     runner = CliRunner()
-    result = runner.invoke(wazotester, [scenario_file])
+    result = runner.invoke(wazotester, [scenario_file, "-t", TARGET])
     assert result.exit_code == 0
